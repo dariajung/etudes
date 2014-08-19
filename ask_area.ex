ba11094b333b3e62294a7edee5e526717b141ef2
Daria Jung <djj2115cu@gmail.com>
2014-08-19 17:48:02 -0400

    update ask_area to take user input

--- ask_area.ex
+++ ask_area.ex
@@ -1,4 +1,18 @@
 defmodule AskArea do
+
+	@spec area() :: number()
+	def area() do
+		inp = IO.gets("R)ectangle T)riangle or E)llipse: ")
+		shape = char_to_shape(String.first(inp))
+
+		{d1, d2} = case shape do
+								 :rectangle -> get_dimensions("length", "width")
+								 :triangle -> get_dimensions("base", "height")
+								 :ellipse -> get_dimensions("major radius", "minor radius")
+								 :unknown -> {0, 0}
+								end
+		calculate(shape, d1, d2)
+	end
 	
 	@spec char_to_shape(char()) :: atom()
 	def char_to_shape(input) do
@@ -21,4 +35,11 @@ defmodule AskArea do
 		{ get_number(prompt1), get_number(prompt2) }
 	end
 
+	def calculate(s, d1, d2) do
+		cond do
+			s == :unknown -> IO.puts("No matching shape, #{s}")
+			d1 < 0 or d2 < 0 -> IO.puts("Can't have negative numbers")
+			true -> Geom.area(s, d1, d2)
+		end
+	end
 end
