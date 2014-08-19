f0988c098b789c7e34dabc5414f29fa2a0c96599
Daria Jung <djj2115cu@gmail.com>
2014-08-19 13:50:21 -0400

    rewrite geom using pattern matching <3

--- geom.ex
+++ geom.ex
@@ -3,27 +3,12 @@ defmodule Geom do
 	@moduledoc """
   Provides functions for calculating areas of geometric shapes.
   """
-	@doc """
-  Calculates the area of a rectangle given w, width, and l, length. 
-  Default values are provided in cases no parameters are passed into
-  the area function.
-  """
-	def area(:rectangle, w, h) when w >= 0 and h >= 0 do
-		w * h
+	def area(shape, a, b) when a >= 0 and b >= 0 do
+		case shape do
+			:rectangle -> a * b
+			:triangle -> a * b / 2.0
+			:ellipse -> a * b * :math.pi()
+			_ -> 0
+		end
 	end
-
-	@doc """
-  Calculates the area of a triangle given a, base, and b, height.
-  """
-  def area(:triangle, a, b) when a >= 0 and b >= 0 do
-		a * b / 2.0
-	end
-	
-	@doc """
-  Calculates the area of an ellipse given a, the major radius, and b,
-  the minor radius 
-  """
-	def area(:ellipse, a, b) when a >= 0 and b >= 0 do
-		a * b * :math.pi()
-  end
 end
