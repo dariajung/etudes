18389c86b08487cca09f3c062360cb4cee121e9b
Daria Jung <djj2115cu@gmail.com>
2014-08-21 13:47:23 -0400

    change order of receive cases in player.ex

--- war/player.ex
+++ war/player.ex
@@ -8,16 +8,16 @@ defmodule Player do
 		# receive action, give x cards, have PID of dealer
 		player_pid = self()
 		receive do
-			{:give, num_cards, dealer_pid} -> 
-				{ payload, new_hand } = Enum.split(hand, num_cards)
-			  	send dealer_pid, {:take, payload, player_pid }
-			  	play(new_hand)
+			:game_over -> IO.puts("Player # {inspect(player_pid)} has left the game.\n")
 			{:collect_loot, loot, dealer_pid} -> 
 				new_hand = hand ++ loot
 			    send dealer_pid, {:received_cards, player_pid}
 				IO.puts("New hand for Player #{inspect(player_pid)} is #{inspect(new_hand)}\n")
 				play(new_hand)
-			:game_over -> IO.puts("Player # {inspect(player_pid)} has left the game.\n")
+			{:give, num_cards, dealer_pid} -> 
+				{ payload, new_hand } = Enum.split(hand, num_cards)
+			  	send dealer_pid, {:take, payload, player_pid }
+			  	play(new_hand)
 		end
 	end
 end
