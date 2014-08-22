c3005e9c17a1eb9b5e90282d4d9024db8268d1ff
Daria Jung <djj2115cu@gmail.com>
2014-08-22 14:38:22 -0400

    error handling

--- stats.ex
+++ stats.ex
@@ -6,10 +6,14 @@ defmodule Stats do
 
 	@spec minimum(list, number()) :: number()
 	def minimum(list, current) do
-		cond do
-			list == [] -> current
-			hd(list) < current -> minimum(tl(list), hd(list))
-			:true -> minimum(tl(list), current)
+		try do
+			cond do
+				list == [] -> current
+				hd(list) < current -> minimum(tl(list), hd(list))
+				:true -> minimum(tl(list), current)
+			end
+		rescue
+			error -> error
 		end
 	end
 
@@ -18,24 +22,36 @@ defmodule Stats do
 	end
 
 	def maximum(list, current) do
-		cond do
-			list == [] -> current
-			hd(list) > current -> maximum(tl(list), hd(list))
-			:true -> maximum(tl(list), current)
+		try do
+			cond do
+				list == [] -> current
+				hd(list) > current -> maximum(tl(list), hd(list))
+				:true -> maximum(tl(list), current)
+			end
+		rescue
+			error -> error
 		end
 	end
 	
 	@spec mean(list(number)) :: number()
 	def mean(list) do
-		sum = List.foldl(list, 0, fn (x, acc) -> x + acc end)
-		sum / Enum.count(list)
+		try do
+			sum = List.foldl(list, 0, fn (x, acc) -> x + acc end)
+			sum / Enum.count(list)
+		rescue
+			error -> error
+		end
 	end
 
 	def stdv(list) do
-		{sum, sum_of_squares} = List.foldl(list, {0, 0}, 
-												 fn (x, {s, sum_sq}) -> {x + s, x * x + sum_sq} end)
-		n = Enum.count(list)
-		temp = (n * sum_of_squares) - (sum * sum)
-		:math.sqrt(temp / (n * (n - 1)))
+		try do
+			{sum, sum_of_squares} = List.foldl(list, {0, 0}, 
+													 fn (x, {s, sum_sq}) -> {x + s, x * x + sum_sq} end)
+			n = Enum.count(list)
+			temp = (n * sum_of_squares) - (sum * sum)
+			:math.sqrt(temp / (n * (n - 1)))
+		rescue
+			error -> error
+		end
 	end
 end
