786105e50a3e407626f9cbbf393b214c7c6021c0
Daria Jung <djj2115cu@gmail.com>
2014-08-23 17:33:24 -0400

    fix parameters

--- kv/lib/kv/bucket.ex
+++ kv/lib/kv/bucket.ex
@@ -1,14 +1,23 @@
 defmodule KV.Bucket do
 
-    def start_link do 
+    @doc """
+    Starts a new bucket.
+    """
+    def start_link do
         Agent.start_link(fn -> HashDict.new end)
     end
 
-    def get(key, bucket) do
+    @doc """
+    Gets a value from the `bucket` by `key`.
+    """
+    def get(bucket, key) do
         Agent.get(bucket, &HashDict.get(&1, key))
     end
 
-    def put(key, value, bucket) do
+    @doc """
+    Puts the `value` for the given `key` in the `bucket`.
+    """
+    def put(bucket, key, value) do
         Agent.update(bucket, &HashDict.put(&1, key, value))
     end
 end
